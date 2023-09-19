af4b32df769dbb23fd33f7cdd6cb52966f17d3de
doney25 <145443681+doney25@users.noreply.github.com>
2023-09-19 16:22:25 +0530

    Division

---
+++ Division.java
@@ -0,0 +1,10 @@
+package javalab;
+
+public class Division {
+	public static void main(String [] args) {
+		
+		float number1=10f,number2=20f;
+		float division=number1/number2;
+		System.out.println("Divison="+division);
+	}
+}
