cac5b79b62262ce8dc26255d18b0305619104b2f
doney25 <145443681+doney25@users.noreply.github.com>
2023-09-19 15:05:50 +0530

    My First Java Program

---
+++ HelloJava.java
@@ -0,0 +1,17 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ File		: HelloJava.java
+ Description: My first Java Project "HelloJava"
+ Author		: Doney Siby
+ Version	: 1.0
+ Date		: 19/09/2023
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+package javalab;
+
+public class HelloJava {
+	public static void main(String [] args) {
+	System.out.println("Hello world");
+	System.out.print("Hello Java");
+	}
+}
