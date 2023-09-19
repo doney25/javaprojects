52e7b4d77c6eb4cec3d0eb6bd7bb2f956f0c5273
doney25 <145443681+doney25@users.noreply.github.com>
2023-09-19 16:23:36 +0530

    AddTwoNumbers.java

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -4,7 +4,6 @@ public class AddTwoNumbers {
 	public static void main(String [] args) {
 	int number1=10,number2=20;
 	int sum=number1+number2;
-	System.out.println("Sum="+sum);
-	
+	System.out.println("Sum="+sum);	
 	}
 }
