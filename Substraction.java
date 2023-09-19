461dcb824ddd3d91f2f96d3af6abe71817edea69
doney25 <145443681+doney25@users.noreply.github.com>
2023-09-19 16:19:17 +0530

    Substraction

---
+++ Substraction.java
@@ -0,0 +1,10 @@
+package javalab;
+
+public class Substraction {
+	public static void main(String [] args) {
+		
+		int number1=10,number2=20;
+		int substraction=number1-number2;
+		System.out.println("Substraction="+substraction);
+}
+}
