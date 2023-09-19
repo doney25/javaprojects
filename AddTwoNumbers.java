84e1941c87d6494f2202b6494577d438ddad7f14
doney25 <145443681+doney25@users.noreply.github.com>
2023-09-19 16:16:53 +0530

    Add files via upload

---
+++ AddTwoNumbers.java
@@ -0,0 +1,10 @@
+package javalab;
+
+public class AddTwoNumbers {
+	public static void main(String [] args) {
+	int number1=10,number2=20;
+	int sum=number1+number2;
+	System.out.println("Sum="+sum);
+	
+	}
+}
