e5f8db5c87294122d6554cbcad3e747ee1bc035c
doney25 <145443681+doney25@users.noreply.github.com>
2023-09-19 16:21:38 +0530

    Multiplication

---
+++ Multiplication.java
@@ -0,0 +1,11 @@
+package javalab;
+
+public class Multiplication{
+public static void main(String [] args) {
+	
+	int number1=10,number2=20;
+	int multiplication=number1*number2;
+	System.out.println("Multiplication="+multiplication);
+
+}
+}
