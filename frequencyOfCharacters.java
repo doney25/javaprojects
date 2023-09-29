196ec3270c2d988b516d163978c2dbc6065c6a38
doney25 <145443681+doney25@users.noreply.github.com>
2023-09-29 16:12:54 +0530

    Frequency of Characters

---
+++ frequencyOfCharacters.java
@@ -0,0 +1,38 @@
+/* File 	  : palindromeOrNot.java
+ * Description: To check the frequency of String
+ * Author	  : Doney Siby
+ * Version	  : 1.0
+ * Date		  : 29/09/2023
+ */
+  
+
+package javalab;
+
+import java.util.Scanner;
+
+public class frequencyOfCharacters {
+	public static void main(String[] args)  
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a String");
+		String input=sc.nextLine();  	//input string
+		System.out.println("Enter the Character");
+		char checkMe=sc.next().charAt(0);	//input character
+		int charCount=checkFreq(input,checkMe);
+		System.out.println("The number of occurences of given characters: "+ charCount);
+	}
+	static int checkFreq(String input, char checkMe)	{
+		char[] charArray=input.toCharArray();
+		int charCount=0;
+		for(int i=0;i<input.length();i++)
+		{
+			if(charArray[i]==checkMe)   {
+				charCount++;
+				
+			}
+		}
+
+		return charCount;
+	}
+	
+}
