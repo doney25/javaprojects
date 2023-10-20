469f3079e55171bf68a32f5572b9550bc5d53b2a
Doney Siby <145443681+doney25@users.noreply.github.com>
2023-10-20 14:41:46 +0530

    Abstraction

---
+++ Abstraction.java
@@ -0,0 +1,37 @@
+package javalab;
+public class Abstraction {
+	public static void main(String [] args) {
+				Rectangle rectangle = new Rectangle();
+				Triangle triangle = new Triangle();
+				Hexagon hexagon = new Hexagon();
+				rectangle.numberOfSides();
+				triangle.numberOfSides();
+				hexagon.numberOfSides();
+	}
+}
+				
+		abstract class Shapes{
+		abstract public void numberOfSides();
+			
+		
+				
+			
+		}
+			class Rectangle extends Shape{
+				public void numberOfSides(){
+					System.out.println("The number of sides of Rectangle is 4");
+				}
+			}
+			class Triangle extends Shape{
+				public void numberOfSides() {
+					System.out.println("The number of sides of Triangle is 3");
+				}
+			}
+			class Hexagon extends Shape{
+				public void numberOfSides() {
+					System.out.println("The number of sides of Hexagon is 6");
+				}
+			
+				
+	}
+
